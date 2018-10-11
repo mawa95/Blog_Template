9360fdd729c5709f29e5927b37fdfba66949282d
mawa95 <mawa95@gmail.com>
2018-10-11 10:43:34 +0200

    done abouteMe for tablet

--- js/About.jsx
+++ js/About.jsx
@@ -7,7 +7,7 @@ export default class AboutMe extends React.Component {
                <div className='aboute_me'>
                    <h3 className='about_title'>Aboute me</h3>
                    <div className='blogger_photo'>
-                       <img src='../images/kaboompics_Young%20woman%20with%20basket%20full%20of%20flowers.jpg'></img>
+                       <img src='../images/kaboompics_Woman%20sitting%20in%20a%20cafe%20alone%20(1).jpg'></img>
                        <p>Hello, my name is Lavander. I am a blogger living in New York. This is my blog, where I post my
                            photos and traveling tips.
                        </p>
